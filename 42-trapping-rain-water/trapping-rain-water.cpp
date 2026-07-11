eca59800b0780a56cf8c38aa21c68a4c101c15de
Mohammad Zaid Wadood <zaidwadood15723@gmail.com>
2026-07-11 05:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (57.71%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,22 +4,30 @@ public:
         int n = height.size();
         if (n == 0) {
             return 0;
-        }
-        vector<int> pref(n);
-        pref[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            pref[i] = max(pref[i - 1], height[i]);
-        }
-        vector<int> suff(n);
-        suff[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suff[i] = max(suff[i + 1], height[i]);
-        }
+        };
+
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
         int total = 0;
-        for (int i = 0; i < n; i++) {
-            int l = pref[i];
-            int r = suff[i];
-            total += min(l, r) - height[i];
+        while (l <= r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lmax) {
+                    lmax = height[l];
+                } else {
+                    total += lmax - height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r] >= rmax){
+                    rmax = height[r];
+                }
+                else {
+                    total+=rmax- height[r];
+                }
+                r--;
+            }
+
         }
         return total;
     }
