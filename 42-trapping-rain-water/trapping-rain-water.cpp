5f73ebcb3ff288d62912e97a83bd21b18d92f99b
Mohammad Zaid Wadood <zaidwadood15723@gmail.com>
2026-07-11 05:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (17.65%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> pref(n);
+        pref[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            pref[i] = max(pref[i - 1], height[i]);
+        }
+        vector<int> suff(n);
+        suff[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = max(suff[i + 1], height[i]);
+        }
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            int l = pref[i];
+            int r = suff[i];
+            total += min(l, r) - height[i];
+        }
+        return total;
+    }
+};
