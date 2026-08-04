897cb70e2d6b0b9c3da398f7cba94f98e88b28be
Mohammad Zaid Wadood <zaidwadood15723@gmail.com>
2026-08-04 11:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (73.83%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,31 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if(!root) return ans;
+        vector<vector<int>> res;
+        if (!root){
+            return res;}
         queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
-            int lvl_size= q.size();
-            vector<int> lvl;
-
-            for(int i=0;i<lvl_size;i++){
-                TreeNode* node =q.front();
+        while (!q.empty()) {
+            int lvlsize = q.size();
+            vector<int> temp;
+            while (lvlsize--) {
+                TreeNode* t = q.front();
                 q.pop();
-                lvl.push_back(node->val);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                temp.push_back(t->val);
+                if (t->left != nullptr) {
+                    q.push(t->left);
+                }
+                if (t->right != nullptr) {
+                    q.push(t->right);
+                }
+                
             }
-            ans.push_back(lvl);
-            }
-            return ans;
-    
-        
+            res.push_back(temp);
+        }
+        return res;
     }
 };
